fadfcec0b3f0c6c807d4e48c8aeb93157bed5f37
Geraldo Junior <junior.bimbo+github@gmail.com>
2023-05-21 23:22:47 -0300

    chore: add datatable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,10 @@
 // Entry point for the build script in your package.json
 import "bootstrap";
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
+import DataTable from 'datatables.net-bs5';
+
+$(document).ready(function () {
+  $('#transactions').DataTable();
+});
